6e08a01dfc21a09c1ba93eea099e8910bf348c83
Anuradha Weeraman <aweeraman@gmail.com>
2013-12-15 22:16:57 +0530

    Changed base URI and recorded baseline apib results

--- src/main/java/com/goalmeister/server/Start.java
+++ src/main/java/com/goalmeister/server/Start.java
@@ -15,7 +15,7 @@ import com.goalmeister.services.PingResource;
  */
 public class Start {
 	// Base URI the Grizzly HTTP server will listen on
-	public static final String BASE_URI = "http://localhost:8080/myapp/";
+	public static final String BASE_URI = "http://localhost:8080/goalmeister/";
 
 	/**
 	 * Starts Grizzly HTTP server exposing JAX-RS resources defined in this
